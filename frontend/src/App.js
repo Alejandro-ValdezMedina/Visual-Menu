9183ffca4e891a5877ba6d001d42d9803a1131a3
Alejandro Valdez Medina <valdezmedina.alejandro@gmail.com>
2025-11-09 12:51:55 -0800

    simple frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [menuItems, setMenuItems] = useState([]);
+  const [generatedImage, setGeneratedImage] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      alert("Please select a file");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    const response = await fetch("http://localhost:8000/api/upload-image", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    setMenuItems(data.items);
+  };
+
+  const handleItemClick = async (itemName) => {
+    const response = await fetch(`http://localhost:8000/api/generate/${itemName}`);
+    const data = await response.json();
+    setGeneratedImage(data.image_url);
+  };
+
+  return (
+    <div className = "App">
+      <h1> Visual Menu</h1>
+      <input
+      type = "file"
+      accept = "image/*"
+      onChange = {handleFileChange}
+      />
+
+      <button onClick = {handleUpload}>
+        Upload Menu
+      </button>
+
+      <div>
+        <h2> Menu Items:</h2>
+        {menuItems.map((item,index) => (
+          <p style = {{cursor: "pointer", color: "blue"}} key = {index} onClick = {() => handleItemClick(item)}>{item}</p>
+        ))}
+      </div>
+      {generatedImage && (
+        <div>
+          <h2> Generated Image:</h2>
+          <img src = {generatedImage} alt = "Generated Image" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
